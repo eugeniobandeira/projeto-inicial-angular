6637c0912ca1c12c95c4abd1f3bae464dbd7c447
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-03-29 11:09:26 -0300

    feat: estilização com o seletor ::ng-deep

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,19 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styles: [
-    `
-      .card-cancel-button {
-        font-size: 16px;
-        font-weight: 600;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: blue;
-        padding: 10px;
-        cursor: pointer;
-      }
-    `,
-  ],
+  styleUrl: './card-button-cancel.component.scss'
 })
 export class CardButtonCancelComponent {}
