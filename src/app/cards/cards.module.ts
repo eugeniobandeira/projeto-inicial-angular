28cfa7046d8a361049da8c348d0307cf0a79d059
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-03-29 10:00:26 -0300

    feat: card estilizado por meio de prop styles

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,22 +4,26 @@ import { CardComponent } from '../card/card.component';
 import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
 
 @NgModule({
   declarations: [
     CardComponent,
     CardRoxoComponent,
     CardButtonComponent,
-    CardRoxoButtonComponent
+    CardRoxoButtonComponent,
+    CardButtonCancelComponent
   ],
   imports: [
     CommonModule,
+
   ],
   exports: [
     CardComponent,
     CardRoxoComponent,
     CardButtonComponent,
-    CardRoxoButtonComponent
+    CardRoxoButtonComponent,
+    CardButtonCancelComponent
   ]
 })
 export class CardsModule { }
